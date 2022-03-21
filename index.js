09eff32bc0eb058218b4ea511b984bbd9e3c3e89
Shawn <shawnmathes8807@gmail.com>
2022-03-20 21:26:40 -0400

    random gif selector

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-const Discord = require('discord.js');
+//const Discord = require('discord.js');
+import Discord from 'discord.js';
 const client = new Discord.Client();
-require('dotenv').config();
+import fetch from 'node-fetch';
+//require('dotenv').config();
+import dotenv from 'dotenv';
 
-//const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+dotenv.config();
+
+client.login(process.env.TOKEN);
 
 client.on('ready', () => {
     console.log('Ready!');
 });
 
-client.on('message', msg => {
-    if(msg.content === 'test') {
-        msg.reply('The fuck you want?');
+client.on('message', async (msg) => {
+    //if(msg.content === 'hi') 
+    const greetings = getGreetings();
+    const sanitizedString = sanitizeString(msg.content);
+    console.log(sanitizedString);
+    if(sanitizedString.split(' ').some(r => greetings.includes(r)))
+    {
+        const url = `https://g.tenor.com/v1/search?q=${getGreetings()[Math.floor(Math.random() * getGreetings().length)]}&key=${process.env.TENOR_API_KEY}&limit=100`;
+
+        const response = await fetch(url);
+        //console.log(response);
+
+        const result = await response.json();
+        //console.log(result);
+
+        const index = Math.floor(Math.random() * result.results.length);
+
+        msg.reply(result.results[index].url);
     }
 })
 
-client.login(process.env.TOKEN);
+const getGreetings = () => {
+    const greetings = [
+        'hey',
+        'hi',
+        'yo', 
+        'sup',
+        'hello'
+    ]
+
+    return greetings;
+}
+
+const sanitizeString = (str) => {
+    return str.toLowerCase().replace(',', '').replace('.', '')
+}
